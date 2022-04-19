4b5ad51b1ff10e9fb3dcb6fc5a859f03b59d17a8
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-04-19 16:55:20 +0530

    React4

--- src/Routing/Student.js
+++ src/Routing/Student.js
@@ -21,7 +21,7 @@ const tableHead = {
 const Student = () => {
   return (
     <div>
-       <div className='top-div'>
+       <div className='container top-div'>
             <div className='div1'>
               Student Details
             </div>
@@ -31,46 +31,48 @@ const Student = () => {
         </div>
 
         <br/>
-       <div className='tab'>
-      <TableContainer style={{ backgroundColor: "black",  borderRadius:"5px"}}>
-      <Table>
-        <TableHead>
-          <TableRow >
-            <TableCell style={tableHead} align="center">Name</TableCell>
-            <TableCell style={tableHead} align="center">Age</TableCell>
-            <TableCell style={tableHead} align="center">Course</TableCell>
-            <TableCell style={tableHead} align="center">Batch</TableCell>
-            <TableCell style={tableHead} align="center">Change</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-            <TableRow >
-              <TableCell style={tableCellS} align="center">Harvey</TableCell>
-              <TableCell style={tableCellS} align="center">24</TableCell>
-              <TableCell style={tableCellS} align="center">MERN</TableCell>
-              <TableCell style={tableCellS} align="center">October</TableCell>
-              <TableCell style={tableCellS} align="center"><button className='edit'>Edit</button></TableCell>
-            </TableRow>
+        <div className='container'>
+        <div className='tab'>
+   <TableContainer style={{ backgroundColor: "black",  borderRadius:"5px"}}>
+   <Table>
+     <TableHead>
+       <TableRow >
+          <TableCell style={tableHead} align="center">Name</TableCell>
+         <TableCell style={tableHead} align="center">Age</TableCell>
+         <TableCell style={tableHead} align="center">Course</TableCell>
+         <TableCell style={tableHead} align="center">Batch</TableCell>
+         <TableCell style={tableHead} align="center">Change</TableCell>
+       </TableRow>
+     </TableHead>
+     <TableBody>
+         <TableRow >
+           <TableCell style={tableCellS} align="center">Harvey</TableCell>
+           <TableCell style={tableCellS} align="center">24</TableCell>
+           <TableCell style={tableCellS} align="center">MERN</TableCell>
+           <TableCell style={tableCellS} align="center">October</TableCell>
+           <TableCell style={tableCellS} align="center"><button className='edit'>Edit</button></TableCell>
+         </TableRow>
 
-            <TableRow >
-              <TableCell style={tableCellS} align="center">Mike</TableCell>
-              <TableCell style={tableCellS} align="center">21</TableCell>
-              <TableCell style={tableCellS} align="center">MERN</TableCell>
-              <TableCell style={tableCellS} align="center">November</TableCell>
-              <TableCell style={tableCellS} align="center"><button className='edit'>Edit</button></TableCell>
-            </TableRow>
+         <TableRow >
+           <TableCell style={tableCellS} align="center">Mike</TableCell>
+           <TableCell style={tableCellS} align="center">21</TableCell>
+           <TableCell style={tableCellS} align="center">MERN</TableCell>
+           <TableCell style={tableCellS} align="center">November</TableCell>
+           <TableCell style={tableCellS} align="center"><button className='edit'>Edit</button></TableCell>
+         </TableRow>
 
-            <TableRow >
-              <TableCell style={tableCellS} align="center">Miguel</TableCell>
-              <TableCell style={tableCellS} align="center">20</TableCell>
-              <TableCell style={tableCellS} align="center">MERN</TableCell>
-              <TableCell style={tableCellS} align="center">December</TableCell>
-              <TableCell style={tableCellS} align="center"><button className='edit'>Edit</button></TableCell>
-            </TableRow>
-              </TableBody>
-            </Table>
-        </TableContainer>
-        </div>     
+         <TableRow >
+           <TableCell style={tableCellS} align="center">Miguel</TableCell>
+           <TableCell style={tableCellS} align="center">20</TableCell>
+           <TableCell style={tableCellS} align="center">MERN</TableCell>
+           <TableCell style={tableCellS} align="center">December</TableCell>
+           <TableCell style={tableCellS} align="center"><button className='edit'>Edit</button></TableCell>
+         </TableRow>
+           </TableBody>
+         </Table>
+     </TableContainer>
+     </div>
+        </div>
     </div>
   )
 }
